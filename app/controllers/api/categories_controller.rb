10ab42a7031c2e3e750b40a77d3d9748ba8071bd
RyanMora <ryan.aimco@gmail.com>
2017-05-31 11:25:50 -0700

    fixed categories controller

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,7 +1,7 @@
 class Api::CategoriesController < ApplicationController
   def index
     category = params[:name].downcase
-    @categories = Category.all.where('lower(category_type) LIKE ?', '%#{category}%')
+    @categories = Category.all.where('lower(type) LIKE ?', '%#{category}%')
     @categories = @categories[0..6]
     render :index
   end
